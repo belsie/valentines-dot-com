8e8106248c67defa95dd0e287048269c9d053469
belle 3 <annabelle.mouat@gmail.com>
2025-01-30 13:53:06 +1000

    put everything in a folder

---
+++ website/script.js
@@ -0,0 +1,4 @@
+document.getElementById("loveButton").addEventListener("click", function() {
+    document.getElementById("surpriseMessage").classList.remove("hidden");
+    alert("You are the best thing that ever happened to me! 💖");
+});
