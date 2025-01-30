25c5d390439bfcd4e9af7a6f0f351574d9890374
belle 3 <annabelle.mouat@gmail.com>
2025-01-30 14:05:31 +1000

    my go now!

--- website/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("loveButton").addEventListener("click", function() {
-    document.getElementById("surpriseMessage").classList.remove("hidden");
-    alert("You are the best thing that ever happened to me! 💖");
-});
